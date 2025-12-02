dd22edb640a8409894a682a180f537c7dbc35ff2
Sachin V S <sachinsvs13@gmail.com>
2025-12-02 23:09:08 +0530

    added get method for the frontend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import ShowTasks from "./ShowTask";
 
 function App() {
   const [handleAddTask, setHandleAddTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const addTask = (formData) => {
     const newTask = formData.get("taskInput");
     setHandleAddTask((prevTasks) => [...prevTasks, newTask]);
   };
-  
-  axios
-    .post("http://localhost:3000/api/v1/tasks", {
-      name: addTask,
-    })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error("There was an error!", error.message);
-    });
 
-  axios.get("http://localhost:3000/api/v1/tasks")
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error("There was an error!", error.message);
-    });
+  // axios
+  //   .post("http://localhost:3000/api/v1/tasks", {
+  //     name: addTask,
+  //   })
+  //   .then((response) => {
+  //     console.log(response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("There was an error!", error.message);
+  //   });
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/tasks")
+      .then((response) => {
+        setTasks(response.data.tasks);
+        console.log(response.data.tasks);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error.message);
+      });
+  }, []);
+  tasks.map((task) => console.log(task.name));
 
   return (
     <>
@@ -45,6 +51,7 @@ function App() {
           <button className="add-task-button">Add Task</button>
         </form>
         <ul className="task-list">
+          {tasks && <pre>{JSON.stringify(tasks, null, 2)}</pre>}
           {handleAddTask.map((task, index) => (
             <li key={index} className="task-item">
               {task}
